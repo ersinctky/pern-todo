acf282f8908e4bd84deff8b986f3bdd781a7e391
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-06-12 20:13:09 +0300

    dotenv package installed and index.js updated

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const pool = require('./db');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+//ROUTES//
+
+//create a todo
+
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1) RETURNING *',
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log('app has started on localhost 5000');
