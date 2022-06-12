5e96eb0f152a5a45bf5fbd990462a535c882e9ef
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-06-12 20:38:00 +0300

    get a todo route added

--- index.js
+++ index.js
@@ -39,6 +39,21 @@ app.get('/todos', async (req, res) => {
   }
 });
 
+//get a todo
+
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [
+      id,
+    ]);
+
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 app.listen(5000, () => {
   console.log('app has started on localhost 5000');
 });
