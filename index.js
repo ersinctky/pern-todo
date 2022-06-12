7f91d0164230db9b70fa8dea08af160f5cc7596f
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-06-12 20:20:26 +0300

    get todos route added

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ app.post('/todos', async (req, res) => {
   }
 });
 
+//get all todos
+
+app.get('/todos', async (req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo');
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 app.listen(5000, () => {
   console.log('app has started on localhost 5000');
 });
